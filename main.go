922080746414aa70c5083418b07678cd825e98b8
kpadania <kpadania@gmail.com>
2023-12-09 11:11:25 -0500

    added templates for home and about page and rendering it from code

--- main.go
+++ main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	n, err := fmt.Fprintf(w, "This is the home page")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(fmt.Sprintf("No of bytes written:%d", n))
+	renderTemplate(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	n, err := fmt.Fprintf(w, "This is the about page")
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("error parsing template", err)
 	}
-	fmt.Println(fmt.Sprintf("No of bytes written:%d", n))
+	return
 }
 
 func main() {
